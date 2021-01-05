26771897b04f1969629172cc339672fa15c3f1ac
enricocid <enrico2588@gmail.com>
2021-01-05 18:05:52 +0100

    Improve preferences
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/iven/vectorify/VectorifyPreferences.kt
+++ project/app/src/main/java/com/iven/vectorify/VectorifyPreferences.kt
@@ -90,9 +90,16 @@ class VectorifyPreferences(context: Context) {
     }
 
     private fun <T : Any> getObjectForType(key: String, type: Type): T? {
-        mPrefs.getString(key, null)?.let { json ->
-            return mMoshi.adapter<T>(type).fromJson(json)
+        val json = mPrefs.getString(key, null)
+        return if (json == null) {
+            null
+        } else {
+            try {
+                mMoshi.adapter<T>(type).fromJson(json)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
         }
-        return null
     }
 }
